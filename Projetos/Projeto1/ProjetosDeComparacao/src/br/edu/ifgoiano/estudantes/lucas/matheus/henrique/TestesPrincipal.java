5e84484b75e4b5a6c6f1c12abda7dcbee54bab84
Henrique Gabriel <henriquegb152019@gmail.com>
2024-09-06 00:03:43 -0300

    Atualizacao

---
+++ Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/TestesPrincipal.java
@@ -0,0 +1,7 @@
+package br.edu.ifgoiano.estudantes.lucas.matheus.henrique;
+
+public class TestesPrincipal {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
