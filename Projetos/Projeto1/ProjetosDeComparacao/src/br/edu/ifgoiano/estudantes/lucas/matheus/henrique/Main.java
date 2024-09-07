da7f0d99d8a80fbbd789b865ca326d906a04c5c1
Henrique Gabriel <henriquegb152019@gmail.com>
2024-09-07 10:49:12 -0300

    Consertando pacotes e atualizando

--- Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/Main.java
+++ Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/Main.java
@@ -1,31 +1,4 @@
-import br.edu.ifgoiano.estudantes.lucas.matheus.henrique.BolhaOtimizado;
+package br.edu.ifgoiano.estudantes.lucas.matheus.henrique;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        GeradorVetores novo = new GeradorVetores();
-        /*
-        OrdenacaoPorInsercao novaOrdenacaoPorInsercao = new OrdenacaoPorInsercao();
-        int[] vetorOrdenadoPorInsercao = novaOrdenacaoPorInsercao.ordenacaoInsercao(novo.vetorAleatorio(100));
-        //exibeArray(vetorOrdenadoPorInsercao);
-
-        OrdenacaoPorSelecao novoOdP = new OrdenacaoPorSelecao();
-        int [] vetorOrdenadoPorSelecao = novoOdP.ordenandoPorSelecao(novo.vetorAleatorio(100));
-        exibeArray(vetorOrdenadoPorSelecao);
-        */
-
-
-        BolhaOtimizado bo = new BolhaOtimizado();
-        bo.bolhaOtimizado(novo.vetorAleatorio(100));
-
-    }
-
-    public static void exibeArray(int[] vetorMantado) {
-        for (int i = 1; i <= vetorMantado.length - 1; i++) {
-            System.out.println(vetorMantado[i]);
-        }
-        System.out.println("\b");
-    }
-
 }
