fd03e7721601ee24861a3a2f8ef9250eb538759e
Matheus <matheusguerra50@gmail.com>
2024-09-07 13:43:09 -0300

    Atualização vetores

--- Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/GeradorVetores.java
+++ Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/GeradorVetores.java
@@ -2,11 +2,40 @@ package br.edu.ifgoiano.estudantes.lucas.matheus.henrique;
 
 public class GeradorVetores {
 
-    public int[] vetorAleatorio (int tamanho){
-        int [] vetor = new int[tamanho];
-        for(int i = 0; i < tamanho; i++){
-            vetor[i]= (int) (Math.random() * 1000000);
+    public int[] vetorAleatorio(int tamanho) {
+        int[] vetor = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = (int) (Math.random() * 1000000);
         }
         return vetor;
     }
+
+    public int[] vetorDecrescente(int tamanho) {
+        int[] vetorParaOrdenar = vetorAleatorio(tamanho);
+        int tamanhoReferenciaVetor = vetorParaOrdenar.length;
+        boolean trocaFeita;
+        for (int i = 0; i < tamanhoReferenciaVetor - 1; i++) {
+            trocaFeita = false;
+            for (int j = 0; j < tamanhoReferenciaVetor - i - 1; j++) {
+                if (vetorParaOrdenar[j] < vetorParaOrdenar[j + 1]) {
+                    int auxi = vetorParaOrdenar[j];
+                    vetorParaOrdenar[j] = vetorParaOrdenar[j + 1];
+                    vetorParaOrdenar[j + 1] = auxi;
+                    trocaFeita = true;
+                }
+            }
+            if (!trocaFeita) {
+                break;
+            }
+        }
+        return vetorParaOrdenar;
+    }
+
+    OrdenacaoPorSelecao novoOrdenacao = new OrdenacaoPorSelecao();
+
+    public int[] vetorCrescente(int tamanho) {
+        int[] vetorAleatorioCriado = vetorAleatorio(tamanho);
+        return novoOrdenacao.ordenandoPorSelecao(vetorAleatorioCriado);
+    }
 }
+
--- Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/Main.java
+++ Projetos/Projeto1/ProjetosDeComparacao/src/br/edu/ifgoiano/estudantes/lucas/matheus/henrique/Main.java
@@ -5,16 +5,15 @@ public class Main {
         GeradorVetores novo = new GeradorVetores();
 
         OrdenacaoPorInsercao novaOrdenacaoPorInsercao = new OrdenacaoPorInsercao();
-        int[] vetorOrdenadoPorInsercao = novaOrdenacaoPorInsercao.ordenacaoInsercao(novo.vetorAleatorio(100));
-        //exibeArray(vetorOrdenadoPorInsercao);
+        novaOrdenacaoPorInsercao.ordenacaoInsercao(novo.vetorAleatorio(100));
 
         OrdenacaoPorSelecao novoOdP = new OrdenacaoPorSelecao();
-        int [] vetorOrdenadoPorSelecao = novoOdP.ordenandoPorSelecao(novo.vetorAleatorio(100));
-        //exibeArray(vetorOrdenadoPorSelecao);
+        novoOdP.ordenandoPorSelecao(novo.vetorAleatorio(100));
 
         BolhaOtimizado bo = new BolhaOtimizado();
         bo.bolhaOtimizado(novo.vetorAleatorio(1000));
     }
+
     public static void exibeArray(int[] vetorMantado) {
         for (int i = 1; i <= vetorMantado.length - 1; i++) {
             System.out.println(vetorMantado[i]);
