b6942ed5c62a7f500699594252f84da2343c9437
Henrique Gabriel <henriquegb152019@gmail.com>
2024-10-10 17:50:45 -0300

    Novo projeto

---
+++ Projetos/Projeto2/projetodelista/src/main/java/br/edu/ifgoiano/estudante/henrique/test.java
@@ -0,0 +1,7 @@
+package br.edu.ifgoiano.estudante.henrique;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo, logo vou fazer uma lista ordenada circular.");
+    }
+}
