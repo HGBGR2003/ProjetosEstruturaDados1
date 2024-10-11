2d95203d2eda54397514b94587a7061ae7f199f9
Henrique Gabriel <henriquegb152019@gmail.com>
2024-10-10 23:06:02 -0300

    Base do projeto para a realizacao da lista circular ordenada simples

--- Projetos/Projeto2/projetodelista/src/main/java/br/edu/ifgoiano/estudante/henrique/test.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifgoiano.estudante.henrique;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Ola Mundo, logo vou fazer uma lista ordenada circular.");
-    }
-}
