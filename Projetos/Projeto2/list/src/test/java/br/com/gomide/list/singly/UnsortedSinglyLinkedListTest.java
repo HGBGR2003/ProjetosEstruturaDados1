877c2f9907b6b98e874eb903eb769e7c8c1a3838
LucasAlvesG <117421570+LucasAlvesG@users.noreply.github.com>
2024-10-24 10:59:18 -0300

    correção erro da virgula

--- Projetos/Projeto2/list/src/test/java/br/com/gomide/list/singly/UnsortedSinglyLinkedListTest.java
+++ Projetos/Projeto2/list/src/test/java/br/com/gomide/list/singly/UnsortedSinglyLinkedListTest.java
@@ -1,256 +1,256 @@
-package br.com.gomide.list.singly;
+// package br.com.gomide.list.singly;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Nested;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Nested;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.DisplayName;
 
-public class UnsortedSinglyLinkedListTest {
+// public class UnsortedSinglyLinkedListTest {
 
-    @Test
-    @DisplayName("retrieve an empty list")
-    public void retrieveAnEmptyList() {
-        UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//     @Test
+//     @DisplayName("retrieve an empty list")
+//     public void retrieveAnEmptyList() {
+//         UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-        String content = list.listContent();
+//         String content = list.listContent();
 
-        assertEquals("null", content);
-    }
+//         assertEquals("null", content);
+//     }
 
-    @Nested
-    @DisplayName("insert action")
-    class Insertion {
+//     @Nested
+//     @DisplayName("insert action")
+//     class Insertion {
 
-        @Test
-        @DisplayName("insert elements at init successfully")
-        public void insertElementsAtInitSuccessfully() {
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("insert elements at init successfully")
+//         public void insertElementsAtInitSuccessfully() {
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.prepend(2);
-            list.prepend(5);
-            list.prepend(8);
-            list.prepend(4);
+//             list.prepend(2);
+//             list.prepend(5);
+//             list.prepend(8);
+//             list.prepend(4);
 
-            String content = list.listContent();
+//             String content = list.listContent();
 
-            assertEquals("4 -> 8 -> 5 -> 2 -> null", content);
-        }
+//             assertEquals("4 -> 8 -> 5 -> 2 -> null", content);
+//         }
 
-        @Test
-        @DisplayName("insert elements at end successfully")
-        public void insertElementsAtEndSuccessfully() {
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("insert elements at end successfully")
+//         public void insertElementsAtEndSuccessfully() {
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(2);
-            list.append(5);
-            list.append(8);
-            list.append(4);
+//             list.append(2);
+//             list.append(5);
+//             list.append(8);
+//             list.append(4);
 
-            String content = list.listContent();
+//             String content = list.listContent();
 
-            assertEquals("2 -> 5 -> 8 -> 4 -> null", content);
-        }
+//             assertEquals("2 -> 5 -> 8 -> 4 -> null", content);
+//         }
 
-        @Test
-        @DisplayName("insert elements successfully")
-        public void insertElementsSuccessfuly() {
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("insert elements successfully")
+//         public void insertElementsSuccessfuly() {
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(2);
-            list.prepend(5);
-            list.prepend(8);
-            list.append(4);
+//             list.append(2);
+//             list.prepend(5);
+//             list.prepend(8);
+//             list.append(4);
 
-            String content = list.listContent();
+//             String content = list.listContent();
 
-            assertEquals("8 -> 5 -> 2 -> 4 -> null", content);
-        }
+//             assertEquals("8 -> 5 -> 2 -> 4 -> null", content);
+//         }
 
-    }
+//     }
 
-    @Nested
-    @DisplayName("find action")
-    class Find {
+//     @Nested
+//     @DisplayName("find action")
+//     class Find {
 
-        @Test
-        @DisplayName("find an existing element")
-        public void findAnExistingElement() {
-            Integer[] elements = new Integer[]{2, 5, 8, 4};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("find an existing element")
+//         public void findAnExistingElement() {
+//             Integer[] elements = new Integer[]{2, 5, 8, 4};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.prepend(elements);
+//             list.prepend(elements);
 
-            Integer value = list.find(5);
+//             Integer value = list.find(5);
 
-            assertEquals(5, value);
-        }
+//             assertEquals(5, value);
+//         }
 
-        @Test
-        @DisplayName("does not find an element")
-        public void doesNotFindAnElement() {
-            Integer[] elements = new Integer[]{2, 5, 8, 4};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("does not find an element")
+//         public void doesNotFindAnElement() {
+//             Integer[] elements = new Integer[]{2, 5, 8, 4};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.prepend(elements);
+//             list.prepend(elements);
 
-            Integer value = list.find(666);
+//             Integer value = list.find(666);
 
-            assertNull(value);
-        }
+//             assertNull(value);
+//         }
 
-    }
+//     }
 
-    @Nested
-    @DisplayName("remove action")
-    class Remove {
+//     @Nested
+//     @DisplayName("remove action")
+//     class Remove {
 
-        @Test
-        @DisplayName("remove an existing element")
-        public void removeAnExistingElement() {
-            Integer[] elements = new Integer[]{2, 5, 8, 4};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("remove an existing element")
+//         public void removeAnExistingElement() {
+//             Integer[] elements = new Integer[]{2, 5, 8, 4};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            boolean removed = list.remove(5);
-            String content = list.listContent();
+//             boolean removed = list.remove(5);
+//             String content = list.listContent();
 
-            assertTrue(removed);
-            assertEquals("2 -> 8 -> 4 -> null", content);
-        }
+//             assertTrue(removed);
+//             assertEquals("2 -> 8 -> 4 -> null", content);
+//         }
 
-        @Test
-        @DisplayName("remove the last element")
-        public void removeTheLastElement() {
-            Integer[] elements = new Integer[]{2, 5, 8, 4};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("remove the last element")
+//         public void removeTheLastElement() {
+//             Integer[] elements = new Integer[]{2, 5, 8, 4};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            boolean removed = list.remove(4);
-            String content = list.listContent();
+//             boolean removed = list.remove(4);
+//             String content = list.listContent();
 
-            assertTrue(removed);
-            assertEquals("2 -> 5 -> 8 -> null", content);
-        }
+//             assertTrue(removed);
+//             assertEquals("2 -> 5 -> 8 -> null", content);
+//         }
 
-        @Test
-        @DisplayName("remove the first element")
-        public void removeTheFirstElement() {
-            Integer[] elements = new Integer[]{2, 5, 8, 4};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("remove the first element")
+//         public void removeTheFirstElement() {
+//             Integer[] elements = new Integer[]{2, 5, 8, 4};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            boolean removed = list.remove(2);
-            String content = list.listContent();
+//             boolean removed = list.remove(2);
+//             String content = list.listContent();
 
-            assertTrue(removed);
-            assertEquals("5 -> 8 -> 4 -> null", content);
-        }
+//             assertTrue(removed);
+//             assertEquals("5 -> 8 -> 4 -> null", content);
+//         }
 
-        @Test
-        @DisplayName("remove the first element of a single list")
-        public void removeTheFirstElementOfASingleList() {
-            Integer[] elements = new Integer[]{2};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("remove the first element of a single list")
+//         public void removeTheFirstElementOfASingleList() {
+//             Integer[] elements = new Integer[]{2};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            boolean removed = list.remove(2);
-            String content = list.listContent();
+//             boolean removed = list.remove(2);
+//             String content = list.listContent();
 
-            assertTrue(removed);
-            assertEquals("null", content);
-        }
+//             assertTrue(removed);
+//             assertEquals("null", content);
+//         }
 
-        @Test
-        @DisplayName("does not remove an element")
-        public void doesNotRemoveAnElement() {
-            Integer[] elements = new Integer[]{2, 5, 8, 4};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("does not remove an element")
+//         public void doesNotRemoveAnElement() {
+//             Integer[] elements = new Integer[]{2, 5, 8, 4};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            boolean removed = list.remove(666);
-            String content = list.listContent();
+//             boolean removed = list.remove(666);
+//             String content = list.listContent();
 
-            assertFalse(removed);
-            assertEquals("2 -> 5 -> 8 -> 4 -> null", content);
-        }
+//             assertFalse(removed);
+//             assertEquals("2 -> 5 -> 8 -> 4 -> null", content);
+//         }
 
-    }
+//     }
 
-    @Nested
-    @DisplayName("navigation action")
-    class Navigate {
+//     @Nested
+//     @DisplayName("navigation action")
+//     class Navigate {
 
-        @Test
-        @DisplayName("navigate through all elements")
-        public void navigateThroughAllElements() {
-            Integer[] elements = new Integer[]{2, 5, 8};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("navigate through all elements")
+//         public void navigateThroughAllElements() {
+//             Integer[] elements = new Integer[]{2, 5, 8};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            Integer element = list.getNextElement();
-            assertEquals(2, element);
+//             Integer element = list.getNextElement();
+//             assertEquals(2, element);
 
-            element = list.getNextElement();
-            assertEquals(5, element);
+//             element = list.getNextElement();
+//             assertEquals(5, element);
 
-            element = list.getNextElement();
-            assertEquals(8, element);
+//             element = list.getNextElement();
+//             assertEquals(8, element);
 
-            element = list.getNextElement();
-            assertNull(element);
-        }
+//             element = list.getNextElement();
+//             assertNull(element);
+//         }
 
-        @Test
-        @DisplayName("navigate through elements and restart navigation pointer")
-        public void navigateThroughElementsAndRestartNavigationPointer() {
-            Integer[] elements = new Integer[]{2, 5, 8};
-            UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//         @Test
+//         @DisplayName("navigate through elements and restart navigation pointer")
+//         public void navigateThroughElementsAndRestartNavigationPointer() {
+//             Integer[] elements = new Integer[]{2, 5, 8};
+//             UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-            list.append(elements);
+//             list.append(elements);
 
-            Integer element = list.getNextElement();
-            assertEquals(2, element);
+//             Integer element = list.getNextElement();
+//             assertEquals(2, element);
 
-            element = list.getNextElement();
-            assertEquals(5, element);
+//             element = list.getNextElement();
+//             assertEquals(5, element);
 
-            list.clearNavigation();
+//             list.clearNavigation();
 
-            element = list.getNextElement();
-            assertEquals(2, element);
+//             element = list.getNextElement();
+//             assertEquals(2, element);
 
-            element = list.getNextElement();
-            assertEquals(5, element);
+//             element = list.getNextElement();
+//             assertEquals(5, element);
 
-            element = list.getNextElement();
-            assertEquals(8, element);
+//             element = list.getNextElement();
+//             assertEquals(8, element);
 
-            element = list.getNextElement();
-            assertNull(element);
-        }
+//             element = list.getNextElement();
+//             assertNull(element);
+//         }
 
-    }
+//     }
 
-    @Test
-    @DisplayName("clear a list")
-    public void clearAListWithElements() {
-        Integer[] elements = new Integer[]{2, 5, 8, 4};
-        UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
+//     @Test
+//     @DisplayName("clear a list")
+//     public void clearAListWithElements() {
+//         Integer[] elements = new Integer[]{2, 5, 8, 4};
+//         UnsortedSinglyLinkedList<Integer> list = new UnsortedSinglyLinkedList<Integer>();
 
-        list.append(elements);
+//         list.append(elements);
 
-        list.clear();
-        String content = list.listContent();
+//         list.clear();
+//         String content = list.listContent();
 
-        assertEquals("null", content);
-    }
+//         assertEquals("null", content);
+//     }
 
-}
+// }
